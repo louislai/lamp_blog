d4946f1d20fde491e7971775082b80f2638f2028
Louis Lai <hoangdung2506@gmail.com>
2015-01-03 21:38:14 +0700

    Fix IE bug

--- views/common/header.php
+++ views/common/header.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="Louis Lai">
+    <!-- Force IE to render the correct style -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 
     <title>Simple Blog by Louis Lai</title>
 
